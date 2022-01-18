d682c5b14f5fbf3854b7b0745e2b781f54bde400
entrpyc <contact@a-angelov.eu>
2022-01-18 23:11:30 +0200

    added quicksort

--- js/quickSort/index.js
+++ js/quickSort/index.js
@@ -1,15 +1,34 @@
-export function quickSort(arr) {
-  return arr
+export function quickSort(arr, startIndex = 0, endIndex = arr.length - 1) {
+  if (startIndex >= endIndex) return
+  if (endIndex - startIndex >= 9) permuteMedianOfThree(arr)
+
+  let midIndex = partition(arr, startIndex, endIndex);
+
+  quickSort(arr, startIndex, midIndex - 1);
+  quickSort(arr, midIndex + 1, endIndex);
 }
 
-// pick pivot
+function partition(arr, startIndex, endIndex) {
+  const pivotVal = arr[endIndex]
+  let index = startIndex
 
-// create partition resolver
+  for (let i = index; i < endIndex; i++) {
+    if (arr[i] < pivotVal) swap(arr, index++, i)
+  }
 
-// iterate over partition
+  swap(arr, index, endIndex)
+  return index;
+}
 
-// if value at index is > than pivot value, swap them
+const swap = (arr, x, y) => [arr[y], arr[x]] = [arr[x], arr[y]]
 
-// move pivot
+function permuteMedianOfThree(arr) {
+  const permute = [0, Math.ceil((arr.length - 1) / 2), arr.length - 1]
 
-// recursively call the partition resolver on both sides of the pivot
+  if (arr[permute[0]] > arr[permute[1]]) swap(arr, permute[0], permute[1])
+  if (arr[permute[2]] < arr[permute[0]]) swap(arr, permute[2], permute[0])
+  if (arr[permute[1]] > arr[permute[2]]) swap(arr, permute[1], permute[2])
+
+  swap(arr, permute[1], permute[2])
+}
+
