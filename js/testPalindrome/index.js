7c2553c5c7704342c09e93e6492fa630d5eb59be
entrpyc <contact@a-angelov.eu>
2022-01-18 16:09:08 +0200

    added solution

--- js/testPalindrome/index.js
+++ js/testPalindrome/index.js
@@ -1,7 +1,33 @@
-function testPalindrome() {
-  // get input
+const form = document.getElementsByTagName('form')[0]
+const input = document.getElementById('input-test-palindrome')
+const result = document.getElementById('title-result')
 
-  // iterate string
+form.addEventListener('submit', testPalindrome)
 
-  // compare with mirrored character
+function testPalindrome(e) {
+  e.preventDefault();
+
+  let value = normalizeString(input.value);
+  let isPalindrome = true;
+
+  const len = value.length;
+
+  // iterate over half of the string
+  for (let i = 0; i < len / 2; i++) {
+    // compare character from the left-half side of string with the opposite right-half side character
+    // if it's different return false
+    if (value[i] !== value[len - 1 - i]) isPalindrome = false;
+  }
+
+
+  result.className = isPalindrome ? 'green' : 'red'
+  result.innerText = resultMessage[isPalindrome]
+  return isPalindrome;
+}
+
+const normalizeString = value => value.normalize().toLowerCase().trim()
+
+const resultMessage = {
+  true: 'The input is a palindrome',
+  false: 'The input is NOT a palindrome',
 }
