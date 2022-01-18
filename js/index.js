2c0cdac49f7c7036ef13461a6a15844f008c9747
entrpyc <contact@a-angelov.eu>
2022-01-18 13:08:11 +0200

    find paragraphs and spans

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-// dev
-function init() {
-  console.log('init')
-}
-
-function swapHtmlTagsInDOMElementFactory() {
+function swapHtmlTagsInDOMElementFactory(DOMElement) {
+  // get DOM element
   
+  // find paragraphs and spans
+  const paragraphs = DOMElement.getElementsByTagName('p')
+  const spans = DOMElement.getElementsByTagName('span')
+
+  debugger
+
+  // swap tag names
+
+  // update DOM
+  
+  // return array of new tags
 }
