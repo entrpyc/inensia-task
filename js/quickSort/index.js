a44b889a938a9b6d4aabf66ffb1d7cbe3aee1ded
entrpyc <contact@a-angelov.eu>
2022-01-18 17:56:54 +0200

    outline steps

--- js/quickSort/index.js
+++ js/quickSort/index.js
@@ -1,3 +1,15 @@
 export function quickSort(arr) {
   return arr
 }
+
+// pick pivot
+
+// create partition resolver
+
+// iterate over partition
+
+// if value at index is > than pivot value, swap them
+
+// move pivot
+
+// recursively call the partition resolver on both sides of the pivot
