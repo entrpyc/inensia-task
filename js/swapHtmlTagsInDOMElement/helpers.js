d6844cc3812e258ef8c71d28e41772861203151e
entrpyc <contact@a-angelov.eu>
2022-01-18 13:58:34 +0200

    copy attributes

--- js/swapHtmlTagsInDOMElement/helpers.js
+++ js/swapHtmlTagsInDOMElement/helpers.js
@@ -1,12 +1,20 @@
+export function copyAttributes(el, newEl) {
+  if(!el.attributes?.length) return
+
+  for (let i = 0; i < el.attributes.length; i++) {
+    newEl.setAttribute(el.attributes[i].nodeName, el.attributes[i].nodeValue)
+  }
+}
+
 export function swapTagName(tag, name) {
-  // create new elemet
+  // create new element
   const newTag = document.createElement(name)
+
   // copy innerHTML
   newTag.innerHTML = tag.innerHTML
 
   // copy attributes
-
-  // copy events
+  copyAttributes(tag, newTag)
   
   // update DOM
   tag.parentNode?.replaceChild(newTag, tag)
